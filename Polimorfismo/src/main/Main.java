b8addb750a1b567068c4c8a565d8460a786a007f
“taynarossauro” <taynararaqueliz@gmail.com>
2024-10-22 20:13:23 -0300

    Add comments to main

--- Polimorfismo/src/main/Main.java
+++ Polimorfismo/src/main/Main.java
@@ -4,18 +4,19 @@ import models.*;
 public class Main {
 
 	public static void main(String[] args) {	
+		
+		//Instanciando atributos da classe MensagemProfessor e utilizando método para enviar e-mail
 
 		MensagemProfessor mensagemProfesor = new MensagemProfessor("Ohata", "estou com dúvidas!");
 		String print_professor = mensagemProfesor.enviarEmail();
 		
+		//Instanciando atributos da classe MensagemAluno e utilizando método para enviar e-mail
 		MensagemAluno mensagemAluno = new MensagemAluno("Carlos Pirico", "parabéns, você tirou 10!");
 		String print_aluno = mensagemAluno.enviarEmail();
 		
-		
+		//Printando testes
 		System.out.println(print_professor);
 		System.out.println(print_aluno);
-
-		
 		
 	}
 
